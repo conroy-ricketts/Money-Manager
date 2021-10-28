5d56f144138572e1cc2d2cd5a998f1c1efffa57d
nader-fares <naderfares@outlook.com>
2021-10-28 19:44:25 -0400

    group transaction cards by date

--- src/screens/TransactionLog.tsx
+++ src/screens/TransactionLog.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View, Text } from 'react-native';
 import TransactionCard, { Transaction } from '../components/TransactionCards';
 
 const styles = StyleSheet.create
@@ -17,14 +17,25 @@ const styles = StyleSheet.create
     },
 });
 
+function DateLabel({ date }: { date: string}): JSX.Element {
+  return (
+    <View style={{ width: 333, height: 40, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: 0 }}>
+      <View style={{flex: 1, height: 2, backgroundColor: '#000', margin: 0, padding: 0}} />
+      <Text style={{justifyContent: 'center', backgroundColor: '#DBDBD9', alignSelf: 'center', margin: '0', padding: 5 }}>{date}</Text>
+      <View style={{flex: 1, height: 2, backgroundColor: '#000', margin: 0, padding: 0}} />
+    </View>
+  )
+}
+
 function TransactionLog() : JSX.Element {
   return (
     <ScrollView style = {styles.screen}>
 
       {/* This following block of code maps an array of our test transactions
       to transaction cards to be rendered */}
-      {testTransactionsAsJSON.map((transactionData) => (
+      {testTransactionsAsJSON.map((transactionData, i) => (
           <View style = {styles.cards}>
+            {i === 0 || transactionData.date !== testTransactionsAsJSON[i-1].date ? <DateLabel date={transactionData.date} /> : null}
               <TransactionCard transaction = {transactionData}/>
           </View>
       ))}
